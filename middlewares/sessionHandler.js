6dccb7695d32b136e103ce282844aff7b0bf2a18
Alejandro Cuartas Marin <cuartasmarin734@gmail.com>
2022-02-02 21:01:55 -0500

    added session handler

---
+++ middlewares/sessionHandler.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken')
+const { request, response } = require('express')
+
+const sessionHandler = (req = request, res = response, next) => {
+    const token = req.cookies.userToken
+    if(!token){
+        return res.status(401).json({
+            msg: 'User is not logged'
+        })
+    }
+    try {
+        const { id } = jwt.verify(token, process.env.JWT_KEY)
+        if(id !== req.params.id){
+            return res.status(401).json({
+                msg: 'You cannot delete this user.'
+            })
+        }
+        next()
+    } catch (error) {
+        return res.status(401).json({
+            msg: 'Unvalid user token',
+            error: error.message
+        })
+    }
+}
+
+module.exports = sessionHandler
